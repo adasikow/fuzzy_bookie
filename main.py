b07053d52cae24d1e505efddff7e3c1d28300ba6
adasikow <adasikow@gmail.com>
2016-04-25 16:19:45 +0200

    Added calculating team forms

--- main.py
+++ main.py
@@ -56,6 +56,58 @@ def get_results_history(results_base, season_start, season_end):
     f.close()
 
 
+def team_won(match):
+    return (match[0] == HOME and match[2] > match[3]) or (match[0] == AWAY and match[2] < match[3])
+
+
+def team_lost(match):
+    return (match[0] == HOME and match[2] < match[3]) or (match[0] == AWAY and match[2] > match[3])
+
+
+def get_number_of_points(match):
+    if team_won(match):
+        return 3
+    elif team_lost(match):
+        return 0
+    else:  #team drew:
+        return 1
+
+
+def get_number_of_points_in_last_games(results_base, number_of_games, team, side=None):
+    results = results_base[team]
+    result = 0
+    it = 0
+    i = 0
+    while i < number_of_games:
+        if side:
+            while results[it][0] != side:
+                it += 1
+
+        result += get_number_of_points(results[it])
+        it += 1
+        i += 1
+
+    return result
+
+
+def get_form_rating(results_base, team):
+    last_5_games_coefficient = 3.0
+    last_10_games_coefficient = 2.0
+    last_15_games_coefficient = 1.0
+    return get_number_of_points_in_last_games(results_base, 5, team) * last_5_games_coefficient / 15.0 + \
+        get_number_of_points_in_last_games(results_base, 10, team) * last_10_games_coefficient / 30.0 + \
+        get_number_of_points_in_last_games(results_base, 15, team) * last_15_games_coefficient / 45.0
+
+
+def get_side_rating(results_base, team, side):
+    last_10_games_coefficient = 3.0
+    last_20_games_coefficient = 2.0
+    last_30_games_coefficient = 1.0
+    return get_number_of_points_in_last_games(results_base, 10, team, side=side) * last_10_games_coefficient / 30.0 + \
+        get_number_of_points_in_last_games(results_base, 20, team, side=side) * last_20_games_coefficient / 60.0 + \
+        get_number_of_points_in_last_games(results_base, 30, team, side=side) * last_30_games_coefficient / 90.0
+
+
 def main():
     results_base = {}
     get_results_history(results_base, '14', '15')
@@ -64,39 +116,61 @@ def main():
     get_results_history(results_base, '11', '12')
     get_results_history(results_base, '10', '11')
 
-    recent_form = {}
-    get_latest_results(recent_form, '14', '15')
-    get_latest_results(recent_form, '13', '14')
-    get_latest_results(recent_form, '12', '13')
-    get_latest_results(recent_form, '11', '12')
-    get_latest_results(recent_form, '10', '11')
+    recent_results = {}
+    get_latest_results(recent_results, '14', '15')
+    get_latest_results(recent_results, '13', '14')
+    get_latest_results(recent_results, '12', '13')
+    get_latest_results(recent_results, '11', '12')
+    get_latest_results(recent_results, '10', '11')
 
-    for home in results_base.keys():
-        for away in results_base[home].keys():
-            for result in results_base[home][away]:
-                print "{0} {1} : {2} {3}".format(home, result[0], result[1], away)
-
-    for team in recent_form.keys():
-        for result in recent_form[team]:
-            if result[0] == HOME:
-                print "{0} {1} : {2} {3}".format(team, result[2], result[3], result[1])
-            else:  # if result[0] == AWAY
-                print "{0} {1} : {2} {3}".format(result[1], result[2], result[3], team)
+    # for home in results_base.keys():
+    #     for away in results_base[home].keys():
+    #         for result in results_base[home][away]:
+    #             print "{0} {1} : {2} {3}".format(home, result[0], result[1], away)
+    #
+    # for team in recent_results.keys():
+    #     for result in recent_results[team]:
+    #         if result[0] == HOME:
+    #             print "{0} {1} : {2} {3}".format(team, result[2], result[3], result[1])
+    #         else:  # if result[0] == AWAY
+    #             print "{0} {1} : {2} {3}".format(result[1], result[2], result[3], team)
 
     team1 = 'Arsenal'
     team2 = 'ManchesterUtd'
 
-    for result in recent_form[team1]:
+    for result in recent_results[team1]:
         if result[0] == HOME:
             print "{0} {1} : {2} {3}".format(team1, result[2], result[3], result[1])
         else:  # if result[0] == AWAY
             print "{0} {1} : {2} {3}".format(result[1], result[2], result[3], team1)
 
-    for result in results_base[team1][team2]:
-        print "{0} {1} : {2} {3}".format(team1, result[0], result[1], team2)
+    # for result in results_base[team1][team2]:
+    #     print "{0} {1} : {2} {3}".format(team1, result[0], result[1], team2)
+    #
+    # for result in results_base[team2][team1]:
+    #     print "{0} {1} : {2} {3}".format(team2, result[0], result[1], team1)
 
-    for result in results_base[team2][team1]:
-        print "{0} {1} : {2} {3}".format(team2, result[0], result[1], team1)
+    print get_number_of_points_in_last_games(recent_results, 5, team1)
+    print get_number_of_points_in_last_games(recent_results, 10, team1)
+    print get_number_of_points_in_last_games(recent_results, 15, team1)
+    print get_number_of_points_in_last_games(recent_results, 5, team1, side=HOME)
+    print get_number_of_points_in_last_games(recent_results, 10, team1, side=HOME)
+    print get_number_of_points_in_last_games(recent_results, 15, team1, side=HOME)
+    print get_number_of_points_in_last_games(recent_results, 5, team1, side=AWAY)
+    print get_number_of_points_in_last_games(recent_results, 10, team1, side=AWAY)
+    print get_number_of_points_in_last_games(recent_results, 15, team1, side=AWAY)
+    print team1 + ':'
+    print get_form_rating(recent_results, team1)
+    print get_side_rating(recent_results, team1, HOME)
+    print get_side_rating(recent_results, team1, AWAY)
+    print team2 + ':'
+    print get_form_rating(recent_results, team2)
+    print get_side_rating(recent_results, team2, HOME)
+    print get_side_rating(recent_results, team2, AWAY)
+
+    for result in recent_results[team2]:
+        if result[0] == AWAY:
+            print "{0} {1} : {2} {3}".format(result[1], result[2], result[3], team2)
 
 
 def fuzzy_example():
@@ -113,10 +187,10 @@ def fuzzy_example():
 
     system.calculate(my_input, my_output)
 
-    print my_output["Aggressiveness"]
+    print "Fuzzy example result: " + str(my_output["Aggressiveness"])
 
 
 if __name__ == "__main__":
-    #main()
+    main()
     fuzzy_example()
 
